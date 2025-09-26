2b54f6669a73e8d53531272af9bdf3a9a1af8d10
MateusMendes0 <matreuusmendes@gmail.com>
2025-09-26 14:45:22 -0300

    feat : Extraindo Data e OCR

--- read.py
+++ read.py
@@ -1,1 +1,22 @@
 # Desenvolva aqui sua atividade
+
+import pytesseract
+import pandas as pd
+from PIL import Image
+import re
+
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+
+imagem = Image.open('fatura.jpg')
+
+texto_extraido = pytesseract.image_to_string(imagem)
+
+texto = texto_extraido.split('\n')
+
+padrao_data = r"Referente a\s+([A-Z]{3}/\d{4})"
+match = re.search(padrao, texto_extraido)
+
+data = {}
+
+data["Mês/Ano"] = [match.group(1)]
